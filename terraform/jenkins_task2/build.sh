eab96d7a6fba79b63961f8b00992fa0aafe7e4dd
Akash Peter P <akashpeter@Akashs-MacBook-Pro.local>
2026-02-20 12:17:06 +0530

    Removed duplicated jenkins folders inside terraform and fixed repo structure

--- terraform/jenkins_task2/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "==============================="
-echo "Jenkins Task-2 Build Execution"
-echo "Triggered on: $(date)"
-echo "Build executed successfully!"
-echo "==============================="
-
